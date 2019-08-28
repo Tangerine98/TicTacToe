3741b359e99a47c658f2acf9d0688177fc50565c
Tangerine98 <menonnandini98@gmail.com>
2019-08-28 19:21:30 +0530

    Updates

---
+++ precache-manifest.334c068a5666fba7ccfc0b7f4272a26c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947e95e99bfe4ddfe10655d70b8cd22e",
+    "url": "/Learning-React/index.html"
+  },
+  {
+    "revision": "1d576812ddb3cae99e16",
+    "url": "/Learning-React/static/css/main.965b29e0.chunk.css"
+  },
+  {
+    "revision": "a2a90afab89761bd2fe8",
+    "url": "/Learning-React/static/js/2.43ca332d.chunk.js"
+  },
+  {
+    "revision": "1d576812ddb3cae99e16",
+    "url": "/Learning-React/static/js/main.b55456d5.chunk.js"
+  },
+  {
+    "revision": "d5497671a03ff4d5682a",
+    "url": "/Learning-React/static/js/runtime~main.36d5a71d.js"
+  }
+]);
